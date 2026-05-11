5f0a8ae207797375ea7459964f717afd22a63bc9
sulisamelia-sketch <sulisamelia256@gmail.com>
2026-05-11 15:19:34 +0700

    Menambahkan destructor pada class anak

--- Agregasi.cpp/anak.cpp
+++ Agregasi.cpp/anak.cpp
@@ -11,4 +11,8 @@ public:
     anak(string pNama) :nama(pNama) {
         cout << "Anak \"" << nama << "\" ada\n";
     }
-    
+    ~anak() {
+        cout << "Anak \"" << nama << "\" tidak ada\n";
+    }
+};
+#endif
