8ee5a1a5685a088a506829e5a0ff1ee6b57c6b79
sulisamelia-sketch <sulisamelia256@gmail.com>
2026-05-11 15:50:16 +0700

    menghubungkan dokter dan pasien

--- Asosiasi.cpp
+++ Asosiasi.cpp
@@ -74,4 +74,9 @@ int main() {
     varDokter1->tambahPasien(varPasien2);
     varDokter2->tambahPasien(varPasien1);
 
-    
+    varDokter1->cetakPasien();
+    varDokter2->cetakPasien();
+    varPasien1->cetakDokter();
+    varPasien2->cetakDokter();
+
+   
