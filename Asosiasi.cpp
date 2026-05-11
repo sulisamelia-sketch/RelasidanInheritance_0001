3fe16938332b59d5d2384e88aa85737a78e7eca2
sulisamelia-sketch <sulisamelia256@gmail.com>
2026-05-11 15:50:50 +0700

    Menghapus pasien dan dokter

--- Asosiasi.cpp
+++ Asosiasi.cpp
@@ -79,4 +79,10 @@ int main() {
     varPasien1->cetakDokter();
     varPasien2->cetakDokter();
 
-   
+    delete varPasien1;
+    delete varPasien2;
+    delete varDokter1;
+    delete varDokter2;
+
+    return 0;
+}
