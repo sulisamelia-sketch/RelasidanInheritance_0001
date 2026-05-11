412fe19315c9a6d40684662a04b71a28294fff23
sulisamelia-sketch <sulisamelia256@gmail.com>
2026-05-11 15:23:32 +0700

    Menambahkan method tambahAnak

--- Agregasi.cpp/ibu.cpp
+++ Agregasi.cpp/ibu.cpp
@@ -18,4 +18,11 @@ public:
     ~ibu() {
         cout << "Ibu \"" << nama << "\" tidak ada\n";
     }
+    void tambahAnak(anak*);
+    void cetakAnak();
+};
+void ibu::tambahAnak(anak* pAnak) {
+    daftar_anak.push_back(pAnak);
+}
+void ibu::cetakAnak() {
     
