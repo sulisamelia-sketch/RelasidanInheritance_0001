d4c72a2b9184e610b1656fb15dc1dcc09b62d0d9
sulisamelia-sketch <sulisamelia256@gmail.com>
2026-05-11 15:37:48 +0700

    Menambahkan relasi agregasi antara ibu dan anak

--- agregasi1.cpp
+++ agregasi1.cpp
@@ -11,4 +11,9 @@ int main() { //agregasi
     anak* varAnak2 = new anak("rini");
     anak* varAnak3 = new anak("Dewi");
 
-    
+    varIbu->tambahAnak(varAnak1);
+    varIbu->tambahAnak(varAnak2);
+    varibu2->tambahAnak(varAnak3);
+    varibu2->tambahAnak(varAnak1);
+
+   
