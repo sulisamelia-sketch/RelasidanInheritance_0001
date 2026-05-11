7a8fbf1d6d587bec87196a6f6429b344cabb7ab4
sulisamelia-sketch <sulisamelia256@gmail.com>
2026-05-11 15:22:37 +0700

    Menambahkan constructor dan destructor class ibu

--- Agregasi.cpp/ibu.cpp
+++ Agregasi.cpp/ibu.cpp
@@ -12,4 +12,10 @@ public:
     string nama;
     vector<anak*> daftar_anak;
 
+    ibu(string pNama) :nama(pNama) {
+        cout << "Ibu \"" << nama << "\" ada\n";
+    }
+    ~ibu() {
+        cout << "Ibu \"" << nama << "\" tidak ada\n";
+    }
     
